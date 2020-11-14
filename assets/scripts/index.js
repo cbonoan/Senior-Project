69c2b6f88bbd55c65e9f121d6889b2ac53f3b43f
Charles Bonoan <charlesbonoan@gmail.com>
2020-11-14 03:13:51 -0800

    working on index

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,8 +1,40 @@
-document.addEventListener("scroll", function() {
-    var elem  = document.getElementById("navbar");
-    if(window.scrollY != 0) {
-        elem.classList.add("shadow");
-    } else {
-        elem.classList.remove("shadow");
-    }
-});
+window.onload = function() {
+    document.addEventListener("scroll", function() {
+        var elem  = document.getElementById("navbar");
+        if(window.scrollY != 0) {
+            elem.classList.add("shadow");
+        } else {
+            elem.classList.remove("shadow");
+        }
+    });
+    
+    var angle = 0; //Initial angle 
+    document.getElementById("custom-toggler").addEventListener("click", function() {
+        var elem = document.getElementById("custom-toggler");
+        elem.style.transitionDuration = "0.5s";
+        if(angle == 0) {
+            elem.style.transform = "rotate(90deg)"; 
+        } else {
+            elem.style.transform = "rotate(0)";
+        }
+        setTimeout(function() {
+            var st = window.getComputedStyle(elem, null);
+            var tr = st.getPropertyValue("-webkit-transform") ||
+                st.getPropertyValue("-moz-transform") ||
+                st.getPropertyValue("-ms-transform") ||
+                st.getPropertyValue("-o-transform") ||
+                st.getPropertyValue("transform");
+            var values = tr.split('(')[1],
+            values = values.split(')')[0],
+            values = values.split(',');
+
+            var a = values[0]; 
+            var b = values[1]; 
+            var c = values[2]; 
+            var d = values[3];
+            angle = Math.round(Math.asin(b) * (180/Math.PI));
+            console.log(angle);
+        },500);
+        
+    });
+}
