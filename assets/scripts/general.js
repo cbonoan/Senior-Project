356e575aa6c75fb4a551aa3904656834efc30b32
Charles Bonoan <charlesbonoan@gmail.com>
2020-11-14 16:46:31 -0800

    working on index page

---
+++ assets/scripts/general.js
@@ -0,0 +1,56 @@
+/*Add file to every HTML page*/
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+function navLinkHover(x) {
+    x.style.borderBottom = "1.5px solid " + getRandomColor();
+}
+function navLinkMouseOut(x) {
+    x.style.borderBottom = 0;
+}
+window.onload = function() {
+    document.addEventListener("scroll", function() {
+        var elem  = document.getElementById("navbar");
+        if(window.scrollY != 0) {
+            elem.classList.add("shadow");
+        } else {
+            elem.classList.remove("shadow");
+        }
+    });
+    
+    var angle = 0; //Initial angle 
+    document.getElementById("custom-toggler").addEventListener("click", function() {
+        var topBar = document.getElementById("topBar");
+        var midBar = document.getElementById("midBar");
+        var bottomBar = document.getElementById("bottomBar");
+        var dropDownElem = document.getElementById("navbarContent");
+        
+        topBar.style.transitionDuration = "0.25s";
+        midBar.style.transitionDuration = "0.25s";
+        bottomBar.style.transitionDuration = "0.25s";
+        bottomBar.style.transformOrigin = "center";
+        topBar.style.transformOrigin = "center";
+        if(window.getComputedStyle(midBar).getPropertyValue("opacity") != 0) {
+            topBar.style.transform = "rotate(45deg)";
+            topBar.style.transform += "translate(0px,18px)";
+            midBar.style.opacity = 0;
+            bottomBar.style.transform = "rotate(-45deg)";
+            bottomBar.style.transform += "translate(0px,-18px)";
+            dropDownElem.style.backgroundColor = "#fbfbff";
+            dropDownElem.style.width - "100%";
+        } else if(window.getComputedStyle(midBar).getPropertyValue("opacity") == 0){
+            topBar.style.transform = "rotate(0deg)";
+            midBar.style.opacity = 1;
+            bottomBar.style.transform = "rotate(0deg)";
+            dropDownElem.style.backgroundColor = "white";
+        }
+
+    });
+
+    
+}
