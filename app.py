8ee4571b2d8d03a06b507652fdfe263489ada514
Charles Bonoan <cbonoan@prosoft-technology.com>
2020-12-03 11:13:22 -0800

    Working with jinja 2 syntax for html pages as well as starting on flask setup -Charles

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-# Main python script. Will rename in future
+from flask import Flask, render_template
+import sys
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    '''
+    For testing purposes, include your IPV4 into the command when running app.py
+    Ex: python app.py <IPV4 address>
+    '''
+    app.run(debug=True, host=sys.argv[1])
