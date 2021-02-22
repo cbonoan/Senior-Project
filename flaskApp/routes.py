5e306fbb404e50e976304b2b14e7083041aa61f0
Charles Bonoan <cbonoan@prosoft-technology.com>
2021-02-22 15:39:57 -0800

    Starting quiz page -Charles

--- flaskApp/routes.py
+++ flaskApp/routes.py
@@ -9,6 +9,14 @@ from flaskApp.models import *
 def home():
     return render_template('index.html')
 
+@app.route("/quiz")
+def quiz():
+    return render_template('quiz.html')
+
+@app.route("/meditation")
+def meditation():
+    return render_template('meditation.html')
+
 @app.route("/register", methods=['GET','POST'])
 def register():
     if request.method == 'POST':
